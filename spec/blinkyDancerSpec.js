b8605a7f6ef8e4b7902dd4d87d3c758beaf2e73f
cihannisanci <cihannisan@gmail.com>
2016-08-02 12:21:13 -0700

    Refactor Dancer and BlinkyDancer to Pseudoclassical style
    Passes tests.

--- spec/blinkyDancerSpec.js
+++ spec/blinkyDancerSpec.js
@@ -5,7 +5,7 @@ describe('blinkyDancer', function() {
 
   beforeEach(function() {
     clock = sinon.useFakeTimers();
-    blinkyDancer = makeBlinkyDancer(10, 20, timeBetweenSteps);
+    blinkyDancer = new BlinkyDancer(10, 20, timeBetweenSteps);
   });
 
   it('should have a jQuery $node object', function() {
@@ -21,11 +21,11 @@ describe('blinkyDancer', function() {
   describe('dance', function() {
     it('should call step at least once per second', function() {
       sinon.spy(blinkyDancer, 'step');
+      
       expect(blinkyDancer.step.callCount).to.be.equal(0);
-      debugger;
-      blinkyDancer = makeBlinkyDancer(10, 20, timeBetweenSteps);
-      blinkyDancer.step();
-      // clock.tick(timeBetweenSteps);
+
+      clock.tick(timeBetweenSteps);
+      // clock.tick(timeBetweenSteps); //it seems an extra tick is necessary...
 
       expect(blinkyDancer.step.callCount).to.be.equal(1);
 
